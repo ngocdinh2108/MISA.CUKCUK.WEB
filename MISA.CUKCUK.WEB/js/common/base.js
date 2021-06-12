cce0c7ae2f9e665f67ddbf672194d82659a91532
DINH DINH NGOC <dndinh@rd.local>
2021-06-12 08:51:39 +0700

    08:51 12.06.2021

--- MISA.CUKCUK.WEB/js/common/base.js
+++ MISA.CUKCUK.WEB/js/common/base.js
@@ -4,23 +4,37 @@ class Base {
     }
 
     loadData(data) {
-        // Lấy thông tin các cột dữ liệu
-        var ths = $('table thead th');
-        var fieldNames = [];
-        // Lấy thông tin dữ liệu sẽ map tương ứng với các cột
-        $.each(data, function(index, obj) {
-            var tr = $(`<tr></tr>`);
-            $.each(ths, function(index, th) {
-                var td = $(`<td></td>`);
-                var fieldName = $(th).attr('fieldName');
-                var value = obj[fieldName];
-                td.append(value);
-                tr.append(td);
-                debugger;
+        try {
+            // Lấy thông tin các cột dữ liệu
+            var ths = $('table thead th');
+            var fieldNames = [];
+            // Lấy thông tin dữ liệu sẽ map tương ứng với các cột
+            $.each(data, function(index, obj) {
+                var tr = $(`<tr></tr>`);
+                $.each(ths, function(index, th) {
+                    var td = $(`<td></td>`);
+                    var fieldName = $(th).attr('fieldName');
+                    var value = obj[fieldName];
+                    var dataType = $(th).attr('dataType');
+                    switch (dataType) {
+                        case 'Date':
+                            value = formatDate(value);
+                            break;
+                        case 'Money':
+                            value = formatMoney(value);
+                            break;
+                        default:
+                            break;
+                    }
+                    td.append(value);
+                    tr.append(td);
+                })
+                $('table tbody').append(tr);
             })
-            $('table tbody').append(tr);
-            debugger;
-        })
+
+        } catch (error) {
+            console.log(error);
+        }
 
     }
 }
