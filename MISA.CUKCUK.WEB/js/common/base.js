0237695def48e58b791bd0a092c215bf23129728
DINH DINH NGOC <dndinh@rd.local>
2021-06-14 17:04:42 +0700

    17:04 14.06.2021

--- MISA.CUKCUK.WEB/js/common/base.js
+++ MISA.CUKCUK.WEB/js/common/base.js
@@ -1,17 +1,95 @@
 class Base {
     constructor() {
-        this.loadData(employees);
+        this.dataJS = null;
+        this.setDataJS();
+        this.loadData();
+        this.initEvents();
     }
 
-    loadData(data) {
+    setDataJS() {
+
+    }
+
+    /**
+     * Hàm khởi tạo sự kiện
+     * DNDINH 14.06.2021
+     */
+    initEvents() {
+        var me = this;
+        // Sự kiện click vào nút thêm mới
+        $('#m-btn-add').click(me.add);
+
+        // Sự kiện click vào nút đóng form
+        $('.dialog-close').click(me.closeForm);
+
+        // Ẩn form khi nhấn button [hủy]
+
+        // Thực hiện lưu dữ liệu khi nhấn button [Lưu] 
+        $('#btn-save').click(me.saveData);
+        // Sự kiện nạp dữ liệu khi ấn button [reload]
+        $('#btn-reload').click(function () {
+            me.loadData();
+        })
+
+        // Hiển thị thông tin chi tiết khi ấn dbl chuột vào 1 dòng
+        $('table tbody').on('dblclick', 'tr', function () {
+            $(".dialog-detail").show();
+        })
+
+        /**
+         * Validate bắt buộc nhập
+         * DNDINH 14.06.2021
+         */
+        $('.input-required').blur(function () {
+            var value = $(this).val();
+            if (!value) {
+                $(this).addClass("border-red");
+                $(this).attr("title", "Trường này không được phép để trống!");
+                $(this).attr("validate", false);
+            }
+            else {
+                $(this).removeClass("border-red");
+                $(this).removeAttr("title");
+                $(this).removeAttr("validate");
+            }
+        })
+
+        /**
+         * Validate email đúng định dạng
+         * DNDINH 14.06.2021
+         */
+        $('input[type="email"]').blur(function () {
+            var value = $(this).val();
+            var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+            if (!mailformat.test(value)) {
+                $(this).addClass("border-red");
+                $(this).attr("title", "Email không đúng định dạng!");
+                $(this).attr("validate", false);
+            }
+            else {
+                $(this).removeClass("border-red");
+                $(this).removeAttr("title");
+                $(this).removeAttr("validate");
+            }
+        })
+
+    }
+
+    /**
+     * Hàm lấy dữ liệu từ file data.js tương ứng với từng trang
+     * DNDINH 14.06.2021
+     */
+    loadData() {
         try {
+            $('table tbody').empty();
+            var data = this.dataJS;
             // Lấy thông tin các cột dữ liệu
             var ths = $('table thead th');
             var fieldNames = [];
             // Lấy thông tin dữ liệu sẽ map tương ứng với các cột
-            $.each(data, function(index, obj) {
+            $.each(data, function (index, obj) {
                 var tr = $(`<tr></tr>`);
-                $.each(ths, function(index, th) {
+                $.each(ths, function (index, th) {
                     var td = $(`<td></td>`);
                     var fieldName = $(th).attr('fieldName');
                     var value = obj[fieldName];
@@ -37,6 +115,82 @@ class Base {
         } catch (error) {
             console.log(error);
         }
+
+    }
+
+    /**
+     * Hàm lưu dữ liệu
+     * DNDINH 14.06.2021
+     */
+    saveData() {
+        // Validate dữ liệu
+        var inputValidates = $('.input-required, input[type="email"]');
+        $.each(inputValidates, function (index, input) {
+            $(input).trigger('blur');
+        })
+        var inputNotValid = $('input[validate="false"]');
+        if (inputNotValid && inputNotValid.length > 0) {
+            alert('Chưa được');
+            inputNotValid[0].focus();
+            return;
+        }
+
+
+        // Thu thập thông tin dữ liệu đc nhập => bulid thành object
+        var employee = {
+            ID: $('#txtID').val(),
+            FullName: $('#txtFullName').val(),
+            Gender: $('#cbxGender').val(),
+            DateOfBirth: $('#dtDateOfBirth').val(),
+            PhoneNumber: $('#txtPhoneNumber').val(),
+            Email: $('#txtEmail').val(),
+            Position: $('#txtPosition').val(),
+            Department: $('#txtDepartment').val(),
+            Salary: $('#txtSalary').val(),
+            Status: $('#txtStatus').val()
+        }
+        // Gọi serviece tương ứng thực hiện lưu trữ
+
+        employees.push(employee);
+        // Sau khi lưu thành công => Đưa ra thông báo
+        alert('thêm thành công');
+        // Ẩn form chi tiết
+        $(".dialog-detail").hide();
+        // Load lai dữ liệu
+        $('#btn-reload').trigger('click');
+    }
+
+    /**
+     * Hàm đóng form
+     * DNDINH 14.06.2021
+     */
+    closeForm() {
+        $(".dialog-detail").hide();
+    }
+
+    /**
+     * Thêm dữ liệu
+     * DNDINH 10.06.2021
+     */
+    add() {
+        var arrayInput = $(".dialog-detail input");
+        $(".dialog-detail").show();
+        arrayInput[0].focus();
+    }
+
+    /**
+     * Sửa dữ liệu
+     * DNDINH 10.06.2021
+     */
+    edit() {
+
+    }
+
+    /**
+     * Xóa dữ liệu
+     * DNDINH 10.06.2021
+     */
+    delete() {
 
     }
 }
--- MISA.CUKCUK.WEB/js/page/employee.js
+++ MISA.CUKCUK.WEB/js/page/employee.js
@@ -1,10 +1,5 @@
-$(document).ready(function() {
+$(document).ready(function () {
     new Employee();
-    dialog = $(".dialog-detail").dialog({
-        autoOpen: false,
-        fluid: true,
-        modal: true
-    });
 })
 
 /**
@@ -12,91 +7,17 @@ $(document).ready(function() {
  * DNDINH 10.06.2021
  */
 class Employee extends Base {
+
+    /**
+     * Hàm khởi tạo đối tượng Employee
+     * DNDINH 14.06.2021
+     */
     constructor() {
         super();
     }
 
-    /**
-     * Load dữ liệu
-     * DNDINH 10.06.2021
-     */
-    // loadData(data) {
-    //     if (data && data.length > 0) {
-    //         let tr = '';
-    //         $.each(data, function(index, item) {
-    //             let dateOfBirth = item.DateOfBirth;
-    //             let salary = item.Salary;
-    //             salary = formatMoney(salary);
-    //             dateOfBirth = formatDate(dateOfBirth);
-    //             tr += `<tr>
-
-    //                         <td>${item.ID}</td>
-    //                         <td>${item.FullName}</td>
-    //                         <td>${item.Gender}</td>
-    //                         <td class="m-align-center">${dateOfBirth}</td>
-    //                         <td>${item.PhoneNumber}</td>
-    //                         <td>${item.Email}</td>
-    //                         <td>${item.Position}</td>
-    //                         <td>${item.Department}</td>
-    //                         <td class="m-align-right">${salary}</td>
-    //                         <td>${item.Status}</td>
-    //                     </tr>`;
-    //         })
-    //         $('table tbody').append(tr);
-    //     }
-    // }
-
-    /**
-     * Thêm dữ liệu
-     * DNDINH 10.06.2021
-     */
-    add() {
-
+    setDataJS(){
+        this.dataJS = employees;
     }
-
-    /**
-     * Sửa dữ liệu
-     * DNDINH 10.06.2021
-     */
-    edit() {
-
-    }
-
-    /**
-     * Xóa dữ liệu
-     * DNDINH 10.06.2021
-     */
-    delete() {
-
-    }
+    
 }
-
-/**
- * Hàm dùng để lấy dữ liệu nhân viên từ data.js
- * DNDINH 09.06.2021
- */
-// function loadData(data) {
-//     if (data && data.length > 0) {
-//         let tr = '';
-//         $.each(data, function(index, item) {
-//             let dateOfBirth = item.DateOfBirth;
-//             let salary = item.Salary;
-//             salary = formatMoney(salary);
-//             dateOfBirth = formatDate(dateOfBirth);
-//             tr += `<tr>
-//                         <td class="m-align-center"><input type="checkbox"/></td>
-//                         <td>${item.ID}</td>
-//                         <td>${item.FullName}</td>
-//                         <td>${item.Gender}</td>
-//                         <td class="m-align-center">${dateOfBirth}</td>
-//                         <td>${item.PhoneNumber}</td>
-//                         <td>${item.Email}</td>
-//                         <td>${item.Position}</td>
-//                         <td>${item.Department}</td>
-//                         <td class="m-align-right">${salary}</td>
-//                         <td>${item.Status}</td>
-//                     </tr>`;
-//         })
-//         $('table tbody').html(tr);
-//     }
-// }
